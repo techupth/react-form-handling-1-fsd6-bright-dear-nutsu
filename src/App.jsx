7d9aac75a99cc5c5aba79519a7a73c0adc1db6bc
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-05 14:10:16 +0700

    feat: add input and display message states and handling update message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [inputMessage, setInputMessage] = useState('');
+  const [displayMessage, setDisplayMessage] = useState('');
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">
+        {displayMessage ? displayMessage : 'Greeting Message'}
+      </div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={inputMessage}
+          onChange={(e) => {
+            setInputMessage(e.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setDisplayMessage(inputMessage);
+            setInputMessage('');
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
